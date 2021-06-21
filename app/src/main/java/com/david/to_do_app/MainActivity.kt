afb04c9ae424864ac5c92e1f289214769ea229dc
erumura <davidenawore@gmail.com>
2021-06-21 10:02:48 -0700

    initial_commit1

---
+++ app/src/main/java/com/david/to_do_app/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.david.to_do_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    val username: String =""
+    val password: String =""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
